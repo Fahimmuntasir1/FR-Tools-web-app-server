f30a48bd25e6e0290191ac680beab0504671c78e
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-23 00:50:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b8pet.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+  } finally {
+    // await client.close();
+  }
+}
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running tools management server");
+});
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
