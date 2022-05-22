19ab3913eb69eb25098a2700cb1dfcb12d9ee799
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-23 01:24:19 +0600

    api created for tools

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b8pet.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,15 +19,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const toolsCollection = client.db("FR-Tools").collection("Tools");
+
+
+    app.get('/tools', async (req, res) => {
+        const query = {}
+        const cursor = toolsCollection.find(query)
+        const tools = await cursor.toArray()
+        res.send(tools)
+    })
+
+
+
   } finally {
     // await client.close();
   }
 }
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 run().catch(console.dir);
 
