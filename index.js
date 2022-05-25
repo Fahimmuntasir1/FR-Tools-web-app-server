b7eafe4f80a7143efd551d7ead08dcd98177aab7
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-25 19:40:18 +0600

    verifyJwt issue solved,, get alluser api created,, tools api created for add items

--- index.js
+++ index.js
@@ -18,21 +18,22 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-function verifyJwt(req, res, next){
-  const authHeader = req.body.authorization
-  if(!authHeader){
-    return res.status(401).send({message : 'unauthorized access'})
-  }
-  const token = authHeader.split(' ')[1]
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-    if(err){
-      return res.status(403).send({message: 'Access Forbidden'})
-    }
-    req.decoded = decoded
-    next()
-  })
-}
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
@@ -91,7 +92,7 @@ async function run() {
     app.put("/update/:id", async (req, res) => {
       const id = req.params.id;
       const object = req.body;
-      
+
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
@@ -114,12 +115,17 @@ async function run() {
       res.send(result);
     });
     //get all user
-    app.get('/user', async(req,res)=>{
-      const users = await userCollection.find().toArray()
-      res.send(users)
-    })
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
 
-
+    // post add items
+    app.post("/tools", async (req, res) => {
+      const newTool = req.body;
+      const result = await toolsCollection.insertOne(newTool);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
