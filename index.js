240d36c09e0f763339e2871f779f310516a95fbf
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-26 20:07:43 +0600

    cors problem

--- index.js
+++ index.js
@@ -15,22 +15,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 //   })
 // );
 
-const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
-app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,authorization"
-  );
-  next();
-});
+// const corsConfig = {
+//   origin: "*",
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
+// app.use(cors(corsConfig));
+// app.options("*", cors(corsConfig));
+// app.use(express.json());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept,authorization"
+//   );
+//   next();
+// });
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b8pet.mongodb.net/?retryWrites=true&w=majority`;
